be790057c6a7fd0937cf8ee1cb8379ca1ac48ce9
Vadadoriya-Harshit <harshitpatelv9@gmail.com>
2025-06-01 19:52:49 +0530

    check now

--- routers/index.js
+++ routers/index.js
@@ -3,10 +3,10 @@ const express = require("express")
 const router = express.Router()
 
 const authRoute = require("./auth")
-const userRoute = require("./user")
+// const userRoute = require("./user")
 
 router.use("/AUTH",authRoute)
-router.use("/user",userRoute)
+// router.use("/user",userRoute)
 
 
 module.exports = router
